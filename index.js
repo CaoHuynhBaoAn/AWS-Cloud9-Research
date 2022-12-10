5af75a75935deab77a447219e73b1ff2c33310b8
taynguyen0206 <phamtrinhtaynguyen2002@gmail.com>
2022-12-10 14:57:14 +0700

    change ipv4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv/config')
 
 const app = require('./config/custom-express');
 
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || '192.168.1.13';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is on at http://${HOST}:${PORT}`));
