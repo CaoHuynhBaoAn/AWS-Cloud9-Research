5eb2c910a6b1ece7954d285eccb65feebdcacb48
taynguyen0206 <phamtrinhtaynguyen2002@gmail.com>
2022-12-10 14:47:26 +0700

    Connecting to  database

--- dao/connectionFactory.js
+++ dao/connectionFactory.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 function connectionFactory() {
   return mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'asd',
+    user: process.env.DB_USER || 'nguyen',
+    password: process.env.DB_PASSWORD || '123456',
     database: process.env.DB_NAME || 'web',
   });
 }
