3fb364f192790138a9f926e7794ca9735385af96
CaoHuynhBaoAn1001 <caoan0012345@gmail.com>
2022-12-18 11:59:01 +0700

    Editing the project

--- index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv/config')
-
-const app = require('./config/custom-express');
-
-const HOST = process.env.HOST || '192.168.1.13';
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Server is on at http://${HOST}:${PORT}`));
